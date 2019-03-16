7716bb41fd07bfd224b1b602415308259d96cfc8
Frai <fraifelipe@gmail.com>
2019-03-15 23:28:17 -0300

    vai

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -16,10 +16,18 @@ namespace Idades.Controllers
             var pessoas = new List<Pessoa>();
             pessoas.Add(new Pessoa("felipe", 21)); 
             pessoas.Add(new Pessoa("carol", 20));
-            pessoas.Add(new Pessoa("maria", 30));
-            pessoas.Add(new Pessoa("clara", 10));
+            pessoas.Add(new Pessoa("maria", 32));
+            pessoas.Add(new Pessoa("clara", 13));
             pessoas.Add(new Pessoa("miguel", 17));
-            pessoas.Add(new Pessoa("joao", 10));
+            pessoas.Add(new Pessoa("joao", 15));
+            pessoas.Add(new Pessoa("carlos", 41)); 
+            pessoas.Add(new Pessoa("samanta", 63));
+            pessoas.Add(new Pessoa("jenifer", 36));
+            pessoas.Add(new Pessoa("eric", 14));
+            pessoas.Add(new Pessoa("evans", 17));
+            pessoas.Add(new Pessoa("andre", 32));
+            pessoas.Add(new Pessoa("eduardo", 70));
+            pessoas.Add(new Pessoa("rafael", 31));
             var pessoa = pessoas.FirstOrDefault(x => x.Nome == nome.ToLower());
             if (pessoa != null)
             {
